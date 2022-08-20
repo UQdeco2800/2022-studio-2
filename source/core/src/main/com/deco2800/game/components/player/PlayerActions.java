6138a3870734075168dd31309b66df7d180c072d
kurt-553 <ku.lucas553@gmail.com>
2022-08-20 16:18:49 +1000

    Changed dash to not stop when stopping walking input

--- source/core/src/main/com/deco2800/game/components/player/PlayerActions.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerActions.java
@@ -37,9 +37,8 @@ public class PlayerActions extends Component {
 
   @Override
   public void update() {
-    if (moving) {
-      updateSpeed();
-    }
+
+    updateSpeed();
   }
 
   private void updateSpeed() {
@@ -80,7 +79,6 @@ public class PlayerActions extends Component {
    */
   void stopWalking() {
     this.walkDirection = Vector2.Zero.cpy();
-    this.dashing = false; // Stop dashing when stop walking
     updateSpeed();
     moving = false;
   }
