7ebafbed74580e6b0869e4144c171fd9cf68c70e
otili9890 <ocarm0809@gmail.com>
2022-08-24 08:11:07 +1000

    Adding TiledMap folder

---
+++ source/core/assets/images/level_1_tiledmap/wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="wall" tilewidth="16" tileheight="32" tilecount="1" columns="1">
+ <image source="../../Users/User/OneDrive/OneDrive Documents/UQ/2022/DECO2800/Map Design/Floor 1/32x32/wall.png" width="17" height="33"/>
+</tileset>
