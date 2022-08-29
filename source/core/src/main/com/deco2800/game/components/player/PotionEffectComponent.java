10aee8940601ba3e06796b355cd5830af15c33f3
jessfroio <jessica.froio@uqconnect.edu.au>
2022-08-29 21:00:10 +1000

    modified javadocs

--- source/core/src/main/com/deco2800/game/components/player/PotionEffectComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/PotionEffectComponent.java
@@ -1,9 +1,7 @@
 package com.deco2800.game.components.player;
 
 import com.badlogic.gdx.physics.box2d.Fixture;
-import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.components.Component;
-import com.deco2800.game.components.PotionStatsComponent;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.physics.BodyUserData;
 import com.deco2800.game.physics.PhysicsLayer;
@@ -13,23 +11,18 @@ import static com.deco2800.game.components.player.PlayerModifier.MOVESPEED;
 
 public class PotionEffectComponent extends Component {
     private short targetLayer;
-    private float knockbackForce = 0f;
-    //private PotionStatsComponent potionStats;
     private HitboxComponent hitboxComponent;
     private String effectType;
     private float effectValue;
 
-
     /**
-     * Create a component which attacks entities on collision, without knockback.
-     *
+     * Component that affects entities on collision.
      * @param targetLayer The physics layer of the target's collider.
      */
 
-    //player layer
     public PotionEffectComponent(short targetLayer, String effectType) {
         this.targetLayer = targetLayer;
-        this.effectType = effectType; // make enum for this?
+        this.effectType = effectType; // make enum for this later
         switch (effectType) {
             case "speed":
                 this.effectValue = 0.25f;
@@ -38,24 +31,34 @@ public class PotionEffectComponent extends Component {
         }
     }
 
+    /**
+     * Creates component attached to potion entity
+     */
     public void create() {
         entity.getEvents().addListener("collisionStart", this::onCollisionStart);
         hitboxComponent = entity.getComponent(HitboxComponent.class);
     }
 
+    /**
+     * Applies component effect on collision
+     * @param me - player entity
+     * @param other - other entity
+     */
     private void onCollisionStart(Fixture me, Fixture other) {
         if (hitboxComponent.getFixture() != me) {
-            // Not triggered by hitbox, ignore
             return;
         }
         if (!PhysicsLayer.contains(targetLayer, other.getFilterData().categoryBits)) {
-            // Doesn't match target layer, ignore
             return;
         }
         Entity target = ((BodyUserData) other.getBody().getUserData()).entity;
         applyEffect(target);
     }
 
+    /**
+     * Applies component effect
+     * @param target - the target entity (player)
+     */
     private void applyEffect(Entity target) {
         PlayerModifier playerModifier = target.getComponent(PlayerModifier.class);
         switch (this.effectType) {
@@ -67,7 +70,7 @@ public class PotionEffectComponent extends Component {
             default:
                 ;
         }
-        // This should be called but for some reason it crashes the game
-        //entity.dispose();
     }
 }
+// This should be called in applyEffect but for some reason it crashes the game
+//entity.dispose();
