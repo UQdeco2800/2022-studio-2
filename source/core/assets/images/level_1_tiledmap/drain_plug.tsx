8401c3f10f526e27d6f1ab935093bb0230483f6a
otili9890 <ocarm0809@gmail.com>
2022-09-05 12:51:16 +1000

    Added plug to map

---
+++ source/core/assets/images/level_1_tiledmap/drain_plug.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="drain_plug" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../Users/User/OneDrive/OneDrive Documents/UQ/2022/DECO2800/Map Design/Floor 1/32x32/drain_plug.png" width="96" height="96"/>
+</tileset>
