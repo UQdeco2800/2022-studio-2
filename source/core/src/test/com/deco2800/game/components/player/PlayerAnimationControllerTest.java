874541d9d3ee9d43c2f3e97955c378b421d8f3a2
IsaacGraham128 <82865705+IsaacGraham128@users.noreply.github.com>
2022-09-07 09:56:04 +1000

    Committing minor changes before merging main into branch

---
+++ source/core/src/test/com/deco2800/game/components/player/PlayerAnimationControllerTest.java
@@ -0,0 +1,23 @@
+package com.deco2800.game.components.player;
+
+import com.deco2800.game.extensions.GameExtension;
+import com.deco2800.game.rendering.AnimationRenderComponent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
+public class PlayerAnimationControllerTest {
+
+//    @BeforeAll
+//    static void beforeAll() {
+//        AnimationRenderComponent = mock(AnimationRenderComponent.class);
+//    }
+
+}
