757877d6610267369e6216790b738ffa37ee1c99
Isaac Graham <isaacgraham128@gmail.com>
2022-09-07 15:51:08 +1000

    Completed all testing functions for the code! Just need to add function comments to main code file

--- source/core/src/main/com/deco2800/game/components/player/PlayerAnimationController.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerAnimationController.java
@@ -11,10 +11,22 @@ public class PlayerAnimationController extends Component {
 
     AnimationRenderComponent animator;
 
+    public static final String MOVEIDLE = "moveIdle";
+    public static final String MOVEUP = "moveUp";
+    public static final String MOVEDOWN = "moveDown";
+    public static final String MOVELEFT = "moveLeft";
+    public static final String MOVELEFTUP = "moveLeftUp";
+    public static final String MOVELEFTDOWN = "moveLeftDown";
+    public static final String MOVERIGHT = "moveRight";
+    public static final String MOVERIGHTUP = "moveRightUp";
+    public static final String MOVERIGHTDOWN = "moveRightDown";
+
     private static final Vector2 LEFTUP = new Vector2(-1f, 1f);
     private static final Vector2 LEFTDOWN = new Vector2(-1f, -1f);
     private static final Vector2 RIGHTUP = new Vector2(1f, 1f);
     private static final Vector2 RIGHTDOWN = new Vector2(1f, -1f);
+
+    private static String currentAnimation;
 
     @Override
     public void create() {
@@ -26,8 +38,11 @@ public class PlayerAnimationController extends Component {
         entity.getEvents().trigger("movementIdle"); // Start out default idle animation
     }
 
+    public String getMovementAnimation() { return currentAnimation; }
+
     private void movementIdle() {
         animator.startAnimation("idle");
+        currentAnimation = MOVEIDLE;
     }
 
     private void handleMovement (Vector2 move) {
@@ -35,20 +50,28 @@ public class PlayerAnimationController extends Component {
         // Cant do a switch for Vector2s UPSIDEDOWNSMILYFACE
         if (move.epsilonEquals(Vector2Utils.UP)) {
             animator.startAnimation("up");
+            currentAnimation = MOVEUP;
         } else if (move.epsilonEquals(Vector2Utils.DOWN)) {
             animator.startAnimation("down");
+            currentAnimation = MOVEDOWN;
         } else if (move.epsilonEquals(Vector2Utils.RIGHT)) {
             animator.startAnimation("right");
+            currentAnimation = MOVERIGHT;
         } else if (move.epsilonEquals(Vector2Utils.LEFT)) {
             animator.startAnimation("left");
+            currentAnimation = MOVELEFT;
         } else if (move.epsilonEquals(RIGHTUP)) {
             animator.startAnimation("up"); // Will change with appropriate diagonal movement sprite
+            currentAnimation = MOVERIGHTUP;
         } else if (move.epsilonEquals(RIGHTDOWN)) {
             animator.startAnimation("down"); // Will change with appropriate diagonal movement sprite
+            currentAnimation = MOVERIGHTDOWN;
         } else if (move.epsilonEquals(LEFTUP)) {
             animator.startAnimation("up"); // Will change with appropriate diagonal movement sprite
+            currentAnimation = MOVELEFTUP;
         } else if (move.epsilonEquals(LEFTDOWN)) {
             animator.startAnimation("down"); // Will change with appropriate diagonal movement sprite
+            currentAnimation = MOVELEFTDOWN;
         }
     }
 }
