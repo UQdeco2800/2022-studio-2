51c21ae7c230e2369e498a4383c769ba5b2d3a83
Lachlan Benson <109946739+LachlanBenson@users.noreply.github.com>
2022-09-07 17:01:26 +1000

    Anti-snipe code

--- source/core/src/main/com/deco2800/game/components/player/PlayerActions.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerActions.java
@@ -220,10 +220,9 @@ public class PlayerActions extends Component {
             ServiceLocator.getEntityService().unregister(enemy);
             //enemy.flagDead();
           }
-          // idk if this is needed? it seems to work without it
-          //enemyCollide = false;
-          //enemyToKill = null;
-
+          // anti-snipe
+          enemyCollide = false;
+          enemyToKill = null;
           break;
         }
       }
