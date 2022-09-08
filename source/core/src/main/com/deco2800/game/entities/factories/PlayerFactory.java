6a62655495d07764d401caac920b02612f66487a
uq-haze <h.suffian@uqconnect.edu.au>
2022-09-08 11:10:57 +1000

    fixed another conflict whoops

--- source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
@@ -60,14 +60,11 @@ public class PlayerFactory {
             .addComponent(inputComponent)
             .addComponent(new PlayerStatsDisplay())
             .addComponent(new OpenCraftingComponent())
-<<<<<<< HEAD
                 .addComponent(new DialogueKeybordInputComponent())
-                .addComponent(new DialogueDisplay());
-
-=======
+                .addComponent(new DialogueDisplay())
             .addComponent(animator)
             .addComponent(new PlayerAnimationController());
->>>>>>> main
+
 
     PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
     player.getComponent(ColliderComponent.class).setDensity(1.5f);
