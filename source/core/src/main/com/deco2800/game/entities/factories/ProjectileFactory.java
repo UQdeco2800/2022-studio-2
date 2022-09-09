efd8fd08754a0b04f1372f1d586f417da8197724
Caitlenn Alsop <caitlenn.alsop@outlook.com>
2022-09-09 12:39:51 +1000

    poop projectiles moving towards target

--- source/core/src/main/com/deco2800/game/entities/factories/ProjectileFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/ProjectileFactory.java
@@ -3,8 +3,12 @@ package com.deco2800.game.entities.factories;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
+import com.deco2800.game.ai.tasks.AITaskComponent;
 import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.components.TouchAttackComponent;
+import com.deco2800.game.components.tasks.ChaseTask;
+import com.deco2800.game.components.tasks.ShootTask;
+import com.deco2800.game.components.tasks.WanderTask;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.physics.PhysicsLayer;
 import com.deco2800.game.physics.PhysicsUtils;
@@ -25,30 +29,31 @@ public class ProjectileFactory {
      * @return the base of projectile
      */
     public static Entity createBaseProjectile() {
+        AITaskComponent aiComponent = new AITaskComponent();
         Entity projectile = new Entity()
                 .addComponent(new PhysicsComponent())
                 .addComponent(new PhysicsMovementComponent())
                 .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE))
+                //.addComponent(new ColliderComponent())
                 .addComponent(new HitboxComponent().setLayer(PhysicsLayer.OBSTACLE))
-                .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 7.5f));
-
-
+                .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 1.5f)) //7.5
+                .addComponent(aiComponent);
         PhysicsUtils.setScaledCollider(projectile, 0.8f, 0.8f);
         projectile.setEntityType(EntityTypes.PROJECTILE);
         return projectile;
 
     }
 
-    public static Entity createPoopsSludge() {
+    public static Entity createPoopsSludge(Entity target) {
         Entity poops = createBaseProjectile();
         poops.addComponent(new CombatStatsComponent(1, 10, 0, 0))
                 .addComponent(new TextureRenderComponent("images/Enemies/poopSludge.png"))
                 .getComponent(TextureRenderComponent.class).scaleEntity();
-        poops.setScale(0.6f, 0.6f);
-
+        poops.setScale(2f, 2f); //0.6
+        poops.getComponent(AITaskComponent.class)
+                .addTask(new ShootTask(target, 10, 10f));
         poops.getComponent(PhysicsMovementComponent.class);
         poops.setEntityType(EntityTypes.ENEMY);
         return poops;
     }
-
 }
