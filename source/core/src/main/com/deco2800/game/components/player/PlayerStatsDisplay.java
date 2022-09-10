b04174b1cd86166d6b5d5b241d62d3f3a2572689
whyhlf <whyhlf@outlook.com>
2022-09-10 22:03:38 +1000

    Basic implementation of stats display

--- source/core/src/main/com/deco2800/game/components/player/PlayerStatsDisplay.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerStatsDisplay.java
@@ -14,10 +14,13 @@ import com.deco2800.game.ui.UIComponent;
  */
 public class PlayerStatsDisplay extends UIComponent {
   Table table;
-  private Image heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/heart.png", Texture.class));
+  private Image heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphics/Health-plunger/plunger_1.png", Texture.class));
+  private Image staminaImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphics/Stamina-tp/tp-stamina_1.png", Texture.class));
+  private Image manaImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphics/Mana-bucket/bucket-mana_1.png", Texture.class));
   private Label healthLabel;
   private Label staminaLabel;
   private Label manaLabel;
+  private Boolean ishealth = false;
 
   /**
    * Creates reusable ui styles and adds actors to the stage.
@@ -44,7 +47,6 @@ public class PlayerStatsDisplay extends UIComponent {
     table.padTop(45f).padLeft(5f);
 
     // Heart image
-    float heartSideLength = 120f;
 //    heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/heart.png", Texture.class));
 
     // Health text
@@ -61,21 +63,21 @@ public class PlayerStatsDisplay extends UIComponent {
     int mana = entity.getComponent(CombatStatsComponent.class).getMana();
     CharSequence manaText = String.format("Mana: %d", mana);
     manaLabel = new Label(manaText, skin, "large");
-    table.add(heartImage).size(heartSideLength).pad(5);
+    table.add(heartImage).size(120, 30).pad(5);
     table.add(healthLabel);
-
-
     stage.addActor(table);
     table = new Table();
     table.top().left();
     table.setFillParent(true);
     table.padTop(90f).padLeft(5f);
+    table.add(staminaImage).size(120, 30).pad(5);
     table.add(staminaLabel);
     stage.addActor(table);
     table = new Table();
     table.top().left();
     table.setFillParent(true);
     table.padTop(120f).padLeft(5f);
+    table.add(manaImage).size(120, 30).pad(5);
     table.add(manaLabel);
     stage.addActor(table);
 
@@ -92,20 +94,54 @@ public class PlayerStatsDisplay extends UIComponent {
    */
   public void updatePlayerHealthUI(int health) {
     dispose();
-
-
-    heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphic/Health_plunger/plunger_1.png", Texture.class));
+    ishealth = true;
+    heartImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphics/Health-plunger/plunger_"+checkImage(health), Texture.class));
     addActors();
+    ishealth =false;
     CharSequence text = String.format("Health: %d", health);
     healthLabel.setText(text);
 
+
   }
+  public String checkImage(int value){
+    String filename= "";
+    if (value ==100){
+      filename= "1.png";
+    }
+    else if(value >=80 &&value<100 ){
+      filename= "2.png";
+    }
+    else if (value <80 && value >=60){
+      filename= "3.png";
+    }
+    else if (value <60 && value >=40){
+      filename= "4.png";
+    }
+    else if (value <40 && value >=20){
+      filename= "5.png";
+    }
+    else if (value <20 && value >=10){
+      filename= "6.png";
+    }
+    else if (value ==0 && ishealth){
+      filename = "8.png";
+    }
+    else if (value <10 && value >=0){
+      filename= "7.png";
+    }
+
+    return filename;
+  }
+
 
   /**
    * Updates the player's stamina on the ui.
    * @param stamina player stamina
    */
   public void updatePlayerStaminaUI(int stamina) {
+    dispose();
+    staminaImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphics/Stamina-tp/tp-stamina_"+checkImage(stamina), Texture.class));
+    addActors();
     CharSequence text = String.format("Stamina: %d", stamina);
     staminaLabel.setText(text);
   }
@@ -115,6 +151,9 @@ public class PlayerStatsDisplay extends UIComponent {
    * @param mana player mana
    */
   public void updatePlayerManaUI(int mana) {
+    dispose();
+    manaImage = new Image(ServiceLocator.getResourceService().getAsset("images/PlayerStatDisplayGraphics/Mana-bucket/bucket-mana_"+checkImage(mana), Texture.class));
+    addActors();
     CharSequence text = String.format("Mana: %d", mana);
     manaLabel.setText(text);
   }
@@ -123,6 +162,8 @@ public class PlayerStatsDisplay extends UIComponent {
   public void dispose() {
     super.dispose();
     heartImage.remove();
+    staminaImage.remove();
+    manaImage.remove();
     healthLabel.remove();
     staminaLabel.remove();
     manaLabel.remove();
