99f391f141485ce994b471e24350c07c6a74867d
eugene2341 <eugenechong2341@gmail.com>
2022-09-28 03:01:18 +1000

    Shows current applied aura on the screen

--- source/core/src/main/com/deco2800/game/components/CombatItemsComponents/WeaponAuraManager.java
+++ source/core/src/main/com/deco2800/game/components/CombatItemsComponents/WeaponAuraManager.java
@@ -3,6 +3,7 @@ package com.deco2800.game.components.CombatItemsComponents;
 import com.deco2800.game.components.Component;
 import com.deco2800.game.components.player.InventoryComponent;
 import com.deco2800.game.entities.Entity;
+import com.deco2800.game.rendering.TextureRenderComponent;
 
 //this is tied to the player
 public class WeaponAuraManager extends Component {
@@ -18,15 +19,25 @@ public class WeaponAuraManager extends Component {
         }
     }
 
+    /**
+     * Check if the aura is expired, if it is reverts the damage stat to default and remove the aura.
+     */
     public void checkAuraEffect() {
         if (auraApplied != null && System.currentTimeMillis() > auraEndTime) {
             weaponStats.setDamage(weaponStats.getDamage() / auraApplied.getComponent(WeaponAuraComponent.class).getDmgMultiplier());
             weaponStats.setCoolDown(weaponStats.getCoolDown() / auraApplied.getComponent(WeaponAuraComponent.class).getCdMultiplier());
             auraApplied = null;
             auraEndTime = 0;
+            //stops showing the applied buff in the UI
+            entity.getComponent(BuffDisplayComponent.class).clearBuff();
         }
     }
 
+    /**
+     * Applies the given aura to the given weapon.
+     * @param aura Aura to apply to the weapon
+     * @param weapon Weapon to apply the aura to
+     */
     public void applyAura(Entity aura, Entity weapon) {
         auraEndTime = System.currentTimeMillis() + aura.getComponent(WeaponAuraComponent.class).getAuraDuration();
         auraApplied = aura;
@@ -34,6 +45,8 @@ public class WeaponAuraManager extends Component {
             weaponStats = weapon.getComponent(PhyiscalWeaponStatsComponent.class);
             weaponStats.setDamage(weaponStats.getDamage() * aura.getComponent(WeaponAuraComponent.class).getDmgMultiplier());
             weaponStats.setCoolDown(weaponStats.getCoolDown() * aura.getComponent(WeaponAuraComponent.class).getCdMultiplier());
+            //shows the applied buff in the game
+            entity.getComponent(BuffDisplayComponent.class).setBuff(aura.getComponent(TextureRenderComponent.class).getTexturePath());
         }
     }
 }
