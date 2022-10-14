7639ff7d7e1a6095c0713d59cb2b91505817fce4
IsaacGraham128 <82865705+IsaacGraham128@users.noreply.github.com>
2022-10-14 15:29:33 +1000

    Reorded loading of services and object to prevent null pointers

--- source/core/src/test/com/deco2800/game/components/player/PlayerStatsDisplayTest.java
+++ source/core/src/test/com/deco2800/game/components/player/PlayerStatsDisplayTest.java
@@ -1,22 +1,12 @@
 package com.deco2800.game.components.player;
+
 import com.badlogic.gdx.assets.AssetManager;
-import com.deco2800.game.services.ResourceService;
-import com.deco2800.game.ui.UIComponent;
-import com.deco2800.game.components.player.PlayerStatsDisplay;
-
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-import com.deco2800.game.components.CombatStatsComponent;
-import com.deco2800.game.components.player.*;
-
-
 import com.deco2800.game.entities.Entity;
+import com.deco2800.game.rendering.RenderService;
+import com.deco2800.game.services.ResourceService;
 import com.deco2800.game.extensions.GameExtension;
-import com.deco2800.game.physics.PhysicsEngine;
-import com.deco2800.game.physics.components.PhysicsComponent;
-import com.deco2800.game.services.GameTime;
 import com.deco2800.game.services.ServiceLocator;
-import org.junit.Assert;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,10 +28,6 @@ public class PlayerStatsDisplayTest{
     @BeforeEach
     void init() {
 
-        dummy = new Entity();
-        playStatDisplay = new PlayerStatsDisplay();
-                //.addComponent(new PlayerStatsDisplay());
-       // display = entity.getComponent(PlayerStatsDisplay.class);
         AssetManager assetManager = spy(AssetManager.class);
         ResourceService resourceService = new ResourceService(assetManager);
         ServiceLocator.registerResourceService(resourceService);
@@ -51,19 +37,25 @@ public class PlayerStatsDisplayTest{
         loads images as textures and for some reason
         below isn't loading them as textures?
          */
+        // hey eli im just leaving this message in, it seems to be the rendering service was null?
+        // I suspect it was due to you calling the playStatDisplay object before you registered
+        // the services and hence its resource service it had was null at the time of creation!
+        // Should be good now :P
         String statImages[] = {"images/PlayerStatDisplayGraphics/Health-plunger/plunger_1.png",
                 "images/PlayerStatDisplayGraphics/Stamina-tp/tp-stamina_1.png",
                 "images/PlayerStatDisplayGraphics/Mana-bucket/bucket-mana_1.png"};
         resourceService.loadTextures(statImages);
         resourceService.loadAll();
 
+        dummy = new Entity();
+        playStatDisplay = new PlayerStatsDisplay();
         dummy.addComponent(playStatDisplay);
     }
 
-    //@Test
+    @Test
     void checkImageTest(){
 
-       // assertEquals("1.png", playStatDisplay.checkImage(100));
+       assertEquals("1.png", playStatDisplay.checkImage(100));
     }
 
     
