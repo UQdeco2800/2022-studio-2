a03254691411e9e395123334948e31df5d7894e6
Ishaan (Ishy) <ishaanranade123@gmail.com>
2022-10-16 19:20:03 +1000

    Added new water designs to lvl 1 map and removed importing of unnecessary image files for map

---
+++ source/core/assets/images/level_1_tiledmap/Water_Animated_Dark.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Water_Animated_Dark" tilewidth="32" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="32x32/frame0waterdark.png"/>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="32x32/frame1waterdark.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="32x32/frame2waterdark.png"/>
+ </tile>
+</tileset>
