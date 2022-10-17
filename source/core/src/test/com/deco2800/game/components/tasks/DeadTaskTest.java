f671b017ce45677a3fc1e9be8b8d38ecdc70287d
MandalorianForce <yi.zhou6@uqconnect.edu.au>
2022-10-17 19:11:23 +1000

    Test supplements

--- source/core/src/test/com/deco2800/game/components/tasks/DeadTaskTest.java
+++ source/core/src/test/com/deco2800/game/components/tasks/DeadTaskTest.java
@@ -3,6 +3,7 @@ package com.deco2800.game.components.tasks;
 import com.deco2800.game.ai.tasks.AITaskComponent;
 import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.entities.Entity;
+import com.deco2800.game.entities.EntityService;
 import com.deco2800.game.events.listeners.EventListener0;
 import com.deco2800.game.physics.PhysicsService;
 import com.deco2800.game.physics.components.HitboxComponent;
@@ -133,4 +134,21 @@ public class DeadTaskTest {
         assertEquals(10, deadTask.getPriority());
     }
 
+    @Test
+    void shouldDisposeAfterStopping() {
+        Entity target = new Entity();
+        DeadTask deadTask = new DeadTask(target, 10);
+        AITaskComponent ai = new AITaskComponent().addTask(deadTask);
+        Entity entity = spy(makePhysicsEntity().addComponent(ai));
+        CombatStatsComponent combatStatsComponent = new CombatStatsComponent(10, 10, 10, 10);
+        entity.addComponent(combatStatsComponent);
+        entity.create();
+
+        EntityService entityService = mock(EntityService.class);
+        ServiceLocator.registerEntityService(entityService);
+        entity.flagDead();
+        entity.update();
+        verify(entity).dispose();
+        verify(entityService).unregister(entity);
+    }
 }
