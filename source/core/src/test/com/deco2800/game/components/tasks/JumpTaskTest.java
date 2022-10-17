30f6236da5b670219940ded6beffa4858b0c49bd
unknown <bach.truong@uqconnect.edu.au>
2022-10-17 20:27:21 +1000

    Adding supporting libraries for Jump Task Test

--- source/core/src/test/com/deco2800/game/components/tasks/JumpTaskTest.java
+++ source/core/src/test/com/deco2800/game/components/tasks/JumpTaskTest.java
@@ -1,6 +1,8 @@
 package com.deco2800.game.components.tasks;
 
 import com.deco2800.game.ai.tasks.AITaskComponent;
+import com.deco2800.game.ai.tasks.DefaultTask;
+import com.deco2800.game.ai.tasks.Task;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.events.listeners.EventListener0;
 import com.deco2800.game.extensions.GameExtension;
