783217297f00c1b35ad0aa666ce553c3a65edaa3
Bokai Zhang <769043056@qq.com>
2022-10-17 18:54:39 +1000

    add dialogue factory test

--- source/core/src/test/com/deco2800/game/entities/factories/NPCFactoryTest.java
+++ source/core/src/test/com/deco2800/game/entities/factories/NPCFactoryTest.java
@@ -1,13 +1,18 @@
 package com.deco2800.game.entities.factories;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.utils.Array;
+import com.deco2800.game.components.npc.NPCAnimationController;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.entities.EntityService;
 import com.deco2800.game.extensions.GameExtension;
 import com.deco2800.game.input.InputService;
 import com.deco2800.game.physics.PhysicsService;
+import com.deco2800.game.rendering.AnimationRenderComponent;
 import com.deco2800.game.rendering.DebugRenderer;
 import com.deco2800.game.rendering.RenderService;
 import com.deco2800.game.services.ResourceService;
@@ -23,6 +28,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(GameExtension.class)
@@ -45,6 +52,8 @@ class NPCFactoryTest {
         ServiceLocator.registerInputService(new InputService());
         ServiceLocator.registerResourceService(new ResourceService());
         ServiceLocator.registerRenderService(new RenderService());
+
+
     }
 
     @Test
@@ -103,5 +112,37 @@ class NPCFactoryTest {
         Entity entity = NPCFactory.creatTestNPC("guard");
         assertTrue(entity.checkEntityType(EntityTypes.NPC));
         assertTrue(entity.checkEntityType(EntityTypes.GUARD));
+    }
+
+    static TextureAtlas createMockAtlas(String animationName, int numRegions) {
+        TextureAtlas atlas = mock(TextureAtlas.class);
+        Array<TextureAtlas.AtlasRegion> regions = new Array<>(numRegions);
+        for (int i = 0; i < numRegions; i++) {
+            regions.add(mock(TextureAtlas.AtlasRegion.class));
+        }
+        when(atlas.findRegions(animationName)).thenReturn(regions);
+        return atlas;
+    }
+
+    @Test
+    void createTest() {
+        Entity entity = new Entity();
+        TextureAtlas atlas = createMockAtlas("MaleShake", 1);
+        AnimationRenderComponent animationRenderComponent = new AnimationRenderComponent(atlas);
+        animationRenderComponent.addAnimation("MaleShake", 1f, Animation.PlayMode.LOOP);
+        entity.addComponent(animationRenderComponent);
+
+        NPCAnimationController npcAnimationController = new NPCAnimationController();
+        npcAnimationController.setEntity(entity);
+        npcAnimationController.create();
+
+        assertEquals(1, entity.getEvents().getNumberOfListeners("MaleShake"));
+        assertEquals(1, entity.getEvents().getNumberOfListeners("femaleShake"));
+        assertEquals(1, entity.getEvents().getNumberOfListeners("childShake"));
+        assertEquals(1, entity.getEvents().getNumberOfListeners("guardShake"));
+        assertEquals(1, entity.getEvents().getNumberOfListeners("humanguardShake"));
+        assertEquals(1, entity.getEvents().getNumberOfListeners("plumberfriendShake"));
+        assertEquals(1, entity.getEvents().getNumberOfListeners("creatureShake"));
+
     }
 }
