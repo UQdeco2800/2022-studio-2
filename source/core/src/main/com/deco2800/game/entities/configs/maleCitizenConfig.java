e6cddccdb15a239f009f4bdcbb8eaee49fa32205
Ishaan (Ishy) <ishaanranade123@gmail.com>
2022-10-17 12:06:33 +1000

    Changed male_citizen to maleCitizen for code smells, removed commented methods for code smells and pushing for implementing changed forest map

---
+++ source/core/src/main/com/deco2800/game/entities/configs/maleCitizenConfig.java
@@ -0,0 +1,8 @@
+package com.deco2800.game.entities.configs;
+
+/**
+ *  Defines the properties stored in male config files to be loaded by the NPC Factory.
+ */
+public class maleCitizenConfig extends BaseEntityConfig{
+    public int baseAttack = 0;
+}
