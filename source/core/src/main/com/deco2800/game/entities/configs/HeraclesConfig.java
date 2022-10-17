c5fe83fd2885de52c19d0bd012912b41962603d1
unknown <bach.truong@uqconnect.edu.au>
2022-10-17 20:37:26 +1000

    Adjust the health of the boss

--- source/core/src/main/com/deco2800/game/entities/configs/HeraclesConfig.java
+++ source/core/src/main/com/deco2800/game/entities/configs/HeraclesConfig.java
@@ -4,7 +4,7 @@ package com.deco2800.game.entities.configs;
  * The statistics that make up the configuration of Heracles, the level 1 boss.
  */
 public class HeraclesConfig extends BaseEntityConfig {
-    public static final int healthHeracles = 10;
+    public static final int healthHeracles = 220;
     public static final int baseAttackHeracles = 10;
     public static final float speed = 2f;
 }
