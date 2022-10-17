7e3f05d740428e1f212eaae7f87fc96db3e1b96f
EverittZ <everittzhang@gmail.com>
2022-10-17 19:11:17 +1000

    Material Test Finished and Most OpenCraftingComponent Test Finished

--- source/core/src/main/com/deco2800/game/components/player/OpenCraftingComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/OpenCraftingComponent.java
@@ -37,7 +37,7 @@ public class OpenCraftingComponent extends Component {
         }
     }
 
-    private boolean inRange(Vector2 playerPosition) {
+    public boolean inRange(Vector2 playerPosition) {
         if (ServiceLocator.getCraftArea().getGameAreaName().equals("Underground")) {
             if (playerPosition.dst(36, 15) < 3 || playerPosition.dst(11, 69) < 3 ||
                     playerPosition.dst(49, 81) < 3 || playerPosition.dst(90, 45) < 3) {
--- source/core/src/test/com/deco2800/game/components/player/OpenCraftingComponentTest.java
+++ source/core/src/test/com/deco2800/game/components/player/OpenCraftingComponentTest.java
@@ -2,10 +2,15 @@ package com.deco2800.game.components.player;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.deco2800.game.areas.ForestGameArea;
 import com.deco2800.game.areas.GameArea;
 import com.deco2800.game.components.gamearea.GameAreaDisplay;
 import com.deco2800.game.entities.Entity;
+import com.deco2800.game.entities.EntityService;
+import com.deco2800.game.entities.factories.PlayerFactory;
 import com.deco2800.game.extensions.GameExtension;
+import com.deco2800.game.input.InputService;
+import com.deco2800.game.physics.PhysicsService;
 import com.deco2800.game.rendering.RenderService;
 import com.deco2800.game.services.ResourceService;
 import com.deco2800.game.services.ServiceLocator;
@@ -15,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
 @ExtendWith(GameExtension.class)
 @ExtendWith(MockitoExtension.class)
 class OpenCraftingComponentTest {
@@ -23,10 +29,145 @@ class OpenCraftingComponentTest {
     OpenCraftingComponent openCraftingComponent;
     @BeforeEach
     void setUp() {
+        ForestGameArea fga = mock(ForestGameArea.class);
+        ServiceLocator.registerGameArea(fga);
+        ServiceLocator.registerEntityService(new EntityService());
+        ServiceLocator.registerPhysicsService(new PhysicsService());
+        ServiceLocator.registerInputService(new InputService());
+        ServiceLocator.registerRenderService(new RenderService());
+        ResourceService resourceService = new ResourceService();
+        ServiceLocator.registerResourceService(resourceService);
+//        AssetManager assetManager = spy(AssetManager.class);
+//        ResourceService resourceService = new ResourceService(assetManager);
+//        ServiceLocator.registerResourceService(resourceService);
+//        GameArea gameArea = spy(GameArea.class);
+//        RenderService renderService = new RenderService();
+//        renderService.setStage(mock(Stage.class));
+//        GameAreaDisplay playerGuidArea = new GameAreaDisplay("Underground");
+//        ServiceLocator.registerGameArea(gameArea);
+//        ServiceLocator.registerRenderService(renderService);
+//        ServiceLocator.registerPlayerGuideArea(playerGuidArea);
+//        ResourceService resourceService1 = spy(ResourceService.class);
+//        ServiceLocator.registerResourceService(resourceService1);
+//        ServiceLocator.registerPhysicsService(new PhysicsService());
+
+        String[] textures = {
+                "images/Crafting-assets-sprint1/crafting table/crafting_inventory_lvl1.png",
+                "images/Crafting-assets-sprint1/crafting table/crafting_inventory_lvl2.png",
+                "images/Crafting-assets-sprint1/widgets/craft_button_lvl1.png",
+                "images/Crafting-assets-sprint1/widgets/craft_button_lvl2.png",
+                "images/Crafting-assets-sprint1/widgets/exit_button_lvl1.png",
+                "images/Crafting-assets-sprint1/widgets/exit_button_lvl2.png",
+                "images/PauseMenu/lvl2PauseScreen.png",
+                "images/PauseMenu/newPauseScreen.png",
+                "images/crafting_assets_sprint2/transparent-texture-buttonClick.png",
+                "images/keybind/level_1/ControlPage.png",
+                "images/keybind/level_2/ControlPage.png",
+                "images/CombatItems/Sprint-3/craftingTeamAssetsNoWhiteSpace/Hera.png",
+                "images/Crafting-assets-sprint1/popups/number1_popup.png",
+                "images/Crafting-assets-sprint1/popups/number2_popup.png",
+                "images/Crafting-assets-sprint1/popups/number3_popup.png",
+                "images/Crafting-assets-sprint1/popups/number4_popup.png",
+                "images/Crafting-assets-sprint1/popups/number5_popup.png",
+                "images/Crafting-assets-sprint1/popups/crafting_indicator.png",
+                "images/Crafting-assets-sprint1/popups/arrow-top-right.png",
+                "images/Crafting-assets-sprint1/popups/arrow-top-left.png",
+                "images/Crafting-assets-sprint1/popups/first-mat-prompt.png",
+                "images/Crafting-assets-sprint1/popups/second-mat-prompt.png",
+                "images/Crafting-assets-sprint1/popups/craft-prompt.png",
+                "images/Crafting-assets-sprint1/crafting table/crafting_catalogue_1_lvl1.png",
+                "images/Crafting-assets-sprint1/crafting table/crafting_catalogue_1_lvl2.png",
+                "images/Crafting-assets-sprint1/widgets/inventory_button_lvl1.png",
+                "images/Crafting-assets-sprint1/widgets/inventory_button_lvl2.png",
+                "images/Crafting-assets-sprint1/widgets/catalogue_page2_lvl1.png",
+                "images/Crafting-assets-sprint1/widgets/catalogue_page2_lvl2.png",
+                "images/Crafting-assets-sprint1/crafting table/crafting_catalogue_2_lvl1.png",
+                "images/Crafting-assets-sprint1/crafting table/crafting_catalogue_2_lvl2.png",
+                "images/Crafting-assets-sprint1/widgets/catalogue_page1_lvl1.png",
+                "images/Crafting-assets-sprint1/widgets/catalogue_page1_lvl2.png",
+//                "sounds/ItemClick.wav",
+//                "sounds/Scroll.wav",
+//                "sounds/new_Weapon_Crafted.wav",
+                "images/CombatItems/Sprint-1/Level 2 Dagger 1.png",
+                "images/CombatItems/Sprint-1/Level 2 Dagger 2png.png",
+                "images/CombatItems/Sprint-1/Enemy_dumbbell.png",
+                "images/CombatItems/Sprint-1/Level 2 Dagger 2png.png",
+                "images/CombatItems/Sprint-1/Sword_Lvl2.png",
+                "images/Crafting-assets-sprint1/materials/gold.png",
+                "images/Crafting-assets-sprint1/materials/rainbow_poop.png",
+                "images/Crafting-assets-sprint1/materials/iron.png",
+                "images/Crafting-assets-sprint1/materials/toilet_paper.png",
+                "images/Crafting-assets-sprint1/materials/steel.png",
+                "images/Crafting-assets-sprint1/materials/wood.png",
+                "images/Crafting-assets-sprint1/materials/plastic.png",
+                "images/Crafting-assets-sprint1/materials/rubber.png",
+                "images/Crafting-assets-sprint1/materials/platinum.png",
+                "images/Crafting-assets-sprint1/materials/silver.png"
+        };
+        resourceService.loadTextures(textures);
+        String[] textureAtlases = {"images/CombatItems/animations/combatItemsAnimation.atlas"};
+        resourceService.loadTextureAtlases(textureAtlases);
+        resourceService.loadAll();
+
+        GameArea gameArea = spy(GameArea.class);
+        RenderService renderService = new RenderService();
+        renderService.setStage(mock(Stage.class));
+        GameAreaDisplay craftingArea = new GameAreaDisplay("Underground");
+        ServiceLocator.registerGameArea(gameArea);
+        ServiceLocator.registerRenderService(renderService);
+        ServiceLocator.registerInventoryArea(craftingArea);
+        craftingArea.create();
+//        playerGuidArea.create();
+        player = new Entity();
+        openCraftingComponent = new OpenCraftingComponent();
+        player.addComponent(openCraftingComponent);
+        player.create();
+        gameArea.setPlayer(player);
+    }
+
+    @Test
+    void shouldCreate() {
+        OpenCraftingComponent openCraftingComponent1 = mock(OpenCraftingComponent.class);
+        openCraftingComponent1.create();
+        verify(openCraftingComponent1).create();
+        assertEquals(false, openCraftingComponent.getCraftingStatus());
+        assertEquals(1, player.getEvents().getNumberOfListeners("can_open"));
+    }
+
+    @Test
+    void shouldOpenCrafting() {
+//        player.setPosition(1111, 15);
+//        player.getEvents().trigger("can_open");
+//        assertTrue(openCraftingComponent.getCraftingStatus());
+//        player.getEvents().trigger("Opening Crafting Menu");
+//        player.getEvents().trigger("Weapon added to inventory");
+//        player.getEvents().trigger("check");
+//        player.getEvents().trigger("Catalogue button pressed");
+//        player.getEvents().trigger("Exit Crafting menu");
+//        OpenCraftingComponent.setCraftingStatus();
+//        assertFalse(openCraftingComponent.getCraftingStatus());
+
+//        Entity entity = PlayerFactory.createTestPlayer();
+//        entity.setPosition(100, 10);
+//        OpenCraftingComponent.setCraftingStatus();
+//        ServiceLocator serviceLocator = mock(ServiceLocator.class);
+//
+//        when(serviceLocator.getGameArea().getPlayer()).thenReturn(entity);
+//
+//        OpenCraftingComponent openCraftingComponent1 = spy(OpenCraftingComponent.class);
+//        openCraftingComponent1.openCrafting();
+//
+//        verify(openCraftingComponent1).openCrafting();
+    }
+
+    @Test
+    void shouldInRange() {
+        player.setPosition(90, 45);
+        assertTrue(openCraftingComponent.inRange(player.getPosition()));
+
         AssetManager assetManager = spy(AssetManager.class);
         ResourceService resourceService = new ResourceService(assetManager);
         ServiceLocator.registerResourceService(resourceService);
-        // Add necessary components to the service locator
         GameArea gameArea = spy(GameArea.class);
         RenderService renderService = new RenderService();
         renderService.setStage(mock(Stage.class));
@@ -34,31 +175,27 @@ class OpenCraftingComponentTest {
         ServiceLocator.registerGameArea(gameArea);
         ServiceLocator.registerRenderService(renderService);
         ServiceLocator.registerPlayerGuideArea(playerGuidArea);
+        ServiceLocator.registerCraftArea(playerGuidArea);
         playerGuidArea.create();
-        // Create appropriate variables exclusive to init
         player = new Entity();
         openCraftingComponent = new OpenCraftingComponent();
         player.addComponent(openCraftingComponent);
         player.create();
-//        gameArea.setPlayer(player);
+        gameArea.setPlayer(player);
+        player.setPosition(100, 10);
+        assertTrue(openCraftingComponent.inRange(player.getPosition()));
+
+        player.setPosition(1111, 1111);
+        assertFalse(openCraftingComponent.inRange(player.getPosition()));
+        assertTrue(openCraftingComponent.getCraftingStatus());
     }
-
-    /*@Test
-    void create() {
-
-    }*/
     @Test
-    void getCraftingStatusTest() {
-        assertEquals(OpenCraftingComponent.getCraftingStatus(), false);
+    void shouldGetCraftingStatus() {
+        assertFalse(openCraftingComponent.getCraftingStatus());
     }
-
     @Test
-    void setCraftingStatus() {
-        if (OpenCraftingComponent.getCraftingStatus()) {
-            OpenCraftingComponent.setCraftingStatus();
-            assertEquals(OpenCraftingComponent.getCraftingStatus(), false);
-        }
+    void shouldSetCraftingStatus() {
+        OpenCraftingComponent.setCraftingStatus();
+        assertTrue(openCraftingComponent.getCraftingStatus());
     }
-
-
 }
