254c2aad18d1a35c80723340728b4135b3173916
rkoll55 <rohankollambalath@gmail.com>
2022-10-17 22:02:52 +1000

    code smell fix

--- source/core/src/main/com/deco2800/game/crafting/CraftingSystem.java
+++ source/core/src/main/com/deco2800/game/crafting/CraftingSystem.java
@@ -22,7 +22,7 @@ public class CraftingSystem implements Runnable{
     /**
      *List containing the users' inventory
      */
-    private static List<Materials> inventoryContents;
+    private static List<Materials> inventoryContents = new ArrayList<>();
 
     /**
      * Constructor that creates an instance of the crafting system class which creates a set of the users inventory and
@@ -33,7 +33,6 @@ public class CraftingSystem implements Runnable{
 
         builtItems = new ArrayList<>();
 
-        inventoryContents = new ArrayList<>();
         inventoryContents.add(Materials.Wood); inventoryContents.add(Materials.Steel);
 
         CraftingLogic.setPossibleBuilds(CraftingLogic.canBuild(inventoryContents));
