d4288aa699bbd5f6577cc7009caf545eef20b3d0
Anders Jepsen <anders1996@live.dk>
2022-10-18 12:08:29 +1000

    ArmourFactory Unit Test

--- source/core/src/test/com/deco2800/game/entities/factories/ArmourFactoryTest.java
+++ source/core/src/test/com/deco2800/game/entities/factories/ArmourFactoryTest.java
@@ -1,5 +1,55 @@
 package com.deco2800.game.entities.factories;
 
-public class ArmourFactoryTest {
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.deco2800.game.components.player.PotionEffectComponent;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.entities.EntityService;
+import com.deco2800.game.extensions.GameExtension;
+import com.deco2800.game.input.InputService;
+import com.deco2800.game.physics.PhysicsLayer;
+import com.deco2800.game.physics.PhysicsService;
+import com.deco2800.game.rendering.DebugRenderer;
+import com.deco2800.game.rendering.RenderService;
+import com.deco2800.game.rendering.TextureRenderComponent;
+import com.deco2800.game.services.ResourceService;
+import com.deco2800.game.services.ServiceLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(GameExtension.class)
+public class ArmourFactoryTest {
+    @Mock
+    ShapeRenderer shapeRenderer;
+    @Mock
+    Box2DDebugRenderer physicsRenderer;
+    @Mock
+    Matrix4 projMatrix;
+    DebugRenderer debugRenderer;
+    @BeforeEach
+    void setUp() {
+        debugRenderer = new DebugRenderer(physicsRenderer, shapeRenderer);
+        ServiceLocator.registerEntityService(new EntityService());
+        ServiceLocator.registerPhysicsService(new PhysicsService());
+        ServiceLocator.registerInputService(new InputService());
+        ServiceLocator.registerResourceService(new ResourceService());
+        ServiceLocator.registerRenderService(new RenderService());
+    }
+
+    @Test
+    void createArmour() {
+        Entity entity = ArmourFactory.createTestArmour();
+        assertTrue(entity.checkEntityType(EntityTypes.ARMOUR));
+    }
 }
